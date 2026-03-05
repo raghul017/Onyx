eaa8f9225dc2c30942539db43f214a7cd8cd874e
Raghul.A.R <arraghul6@gmail.com>
2026-03-05 11:19:39 +0530

    feat: add Vercel Analytics tracking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,6 +4,7 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useServerStatus } from "./store/useServerStatus";
 
@@ -70,6 +71,7 @@ const App = () => {
                         </Routes>
                     </Suspense>
                 </BrowserRouter>
+                <Analytics />
             </TooltipProvider>
         </QueryClientProvider>
     );
