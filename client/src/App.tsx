c83aa5c9a3c32d3431ce5139af5daade9112029e
Raghul.A.R <arraghul6@gmail.com>
2026-03-16 23:54:12 +0530

    feat: integrate Vercel Speed Insights

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import ProtectedRoute from "./components/ProtectedRoute";
 import { useServerStatus } from "./store/useServerStatus";
 
@@ -72,6 +73,7 @@ const App = () => {
                     </Suspense>
                 </BrowserRouter>
                 <Analytics />
+                <SpeedInsights />
             </TooltipProvider>
         </QueryClientProvider>
     );
